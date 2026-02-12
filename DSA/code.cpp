2b3ad261063e33100bd6a2198d9c677b25b46c31
unknown <manjhi7482@gmail.com>
2026-02-12 14:09:23 +0530

    my first commit

---
+++ DSA/code.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 3;
+    int sum = 0;
+    while(int <= n) {
+        cout << sum<<" ";
+        sum += 1;
+    }
+    cout<< endl;
+    return 0;
+}
+    
+    
+
+
+
+ 
